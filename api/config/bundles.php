eb789b94af1c213d31891ba925c6b8bd229a967d
Lilia Ramalho Martins <lilia.martins@gmail.com>
2024-01-09 09:32:25 -0500

    installed lexik package

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
